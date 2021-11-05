7b817ef1b1f72edbd17c560fbd4971d1eb97b8ed
dayraquin <dq53@nau.edu>
2021-11-05 11:33:24 -0700

    Averaging the sums
    The changes made was using an If-statement that will sum up the pokemon, the HP, and Attack points.
    After counting those, the sum of lines and the average of HP and Attack would be printed.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,26 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS = "\t"}
+{
+	if(NR!=1){
+		# Add up the Number of HP listed
+		hp+=$6
+		# count the number of lines 
+		sum+=1
+		# count the attack
+		attack+=$7
+
+	}
+}
+END{
+	# Print the total Pokemon
+	print("Total Pokemon: " sum)
+
+	# print the HP average
+	print("Avg. HP: " hp/sum)
+
+	# print the Attack Average
+	print("Avg. Attack: " attack/sum)
+}
